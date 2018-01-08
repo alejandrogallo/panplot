2cba97cd13b8584daafbedbfc4083e171aa0085c
Alejandro Gallo <aamsgallo@gmail.com>
2018-01-08 23:12:10 +0100

    Update config files

--- panplot/__init__.py
+++ panplot/__init__.py
@@ -1,1 +1,4 @@
 __version__ = '0.0.1'
+__author__ = 'Alejandro Gallo'
+__email__ = 'aamsgallo@gmail.com'
+__license__ = 'GPLv3'
--- setup.py
+++ setup.py
@@ -5,42 +5,89 @@
 #   ./venv/bin/pip install --editable .
 #   ./venv/bin/pip install --editable .[dev]  # with dev requirements, too
 
-from __future__ import print_function
+import sys
+
+main_dependencies = [
+    "setuptools",
+]
+
+for dep in main_dependencies:
+    try:
+        __import__(dep)
+    except ImportError:
+        print(
+            "Error: You do not have %s installed, please\n"
+            "       install it. For example doing\n"
+            "\n"
+            "       pip3 install %s\n" % (dep, dep)
+        )
+        sys.exit(1)
+
 
 from setuptools import setup
-
-from panplot import __version__
-import re
-
-def get_requirements(reqs="requirements.txt"):
-    reqs = [
-        line
-        for line in open(reqs).read().split("\n")[0:-1]
-        if not re.match(r"git\+", line)
-    ]
-    print(reqs)
-    return reqs
+import panplot
 
 
 setup(
     name='panplot',
-    version=__version__,
+    version=panplot.__version__,
     maintainer='Alejandro Gallo',
     maintainer_email='aamsgallo@gmail.com',
-    license='GPLv3',
+    author=panplot.__author__,
+    author_email=panplot.__email__,
+    license=panplot.__license__,
     url='https://github.com/alejandrogallo/panplot',
-    install_requires=get_requirements(),
+    install_requires=[
+        "configparser>=3.0.0",
+    ],
+    python_requires='>=3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Environment :: Console :: Curses',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Utilities',
+    ],
     extras_require=dict(
-        dev=[]
+        # List additional groups of dependencies here (e.g. development
+        # dependencies). You can install these using the following syntax,
+        # for example:
+        # $ pip install -e .[develop]
+        develop=[
+            "sphinx",
+            'sphinx-argparse',
+            'sphinx_rtd_theme',
+            'pytest',
+        ]
     ),
-    description='Simple program to plot',
-    long_description='Simple program to plot',
+    description='Powerful and highly extensible command-line based plotting '
+                'suite',
+    long_description='',
     keywords=[
-        'plot'
+        'plot',
+    ],
+    data_files=[
+
+        ("share/doc/panplot/", [
+            "README.md",
+            "AUTHORS",
+            "LICENSE.txt",
+        ]),
+
     ],
     packages=[
         "panplot",
-        "panplot.commands"
+        "panplot.commands",
     ],
     test_suite="panplot.tests",
     entry_points=dict(
@@ -48,5 +95,5 @@ setup(
             'panplot=panplot.main:main'
         ]
     ),
-    platforms=['linux'],
+    platforms=['linux', 'osx'],
 )
