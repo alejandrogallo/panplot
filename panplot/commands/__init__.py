df85ef926b3bb0c9081e0ed634d8a02cb459d09c
Alejandro Gallo <aamsgallo@gmail.com>
2018-01-08 23:13:03 +0100

    Update commands init from papis

--- panplot/commands/__init__.py
+++ panplot/commands/__init__.py
@@ -1,18 +1,18 @@
-import os
-import sys
-import glob
 import logging
+
+logger = logging.getLogger("commands")
+logger.debug("importing")
+
+import argparse
+import sys
+import os
 import panplot.utils
 import panplot.config
-import argparse
-import argcomplete
-
 COMMAND_NAMES = [
     "default",
     "p2d",
 ]
 
-logger = logging.getLogger("commands")
 DEFAULT_PARSER = None
 SUBPARSERS = None
 COMMANDS = None
@@ -20,30 +20,79 @@ ARGS = None
 
 
 def set_args(args):
+    """
+    Set general command line arguments, this can be used also for testing.
+
+    :param args: Arguments
+    :type  args: Argument object
+    """
     global ARGS
     global logger
     logger.debug("Setting args")
-    if ARGS is None:
-        ARGS = args
+    ARGS = args
 
 
 def set_commands(commands):
+    """
+    Set general initialized commands.
+
+    :param commands: List of initialized command objects
+    :type  commands: List
+    """
     global COMMANDS
     logger.debug("Setting commands")
     COMMANDS = commands
 
 
 def get_commands(command=None):
+    """
+    Get general initialized commands.
+
+    :param command: Command that should be returned.
+    :type  command: str
+    >>> get_commands() is not None
+    True
+    >>> type(get_commands()) is dict
+    True
+    >>> 'add' in get_commands().keys()
+    True
+    """
     global COMMANDS
+    if COMMANDS is None:
+        init_commands()
     if command is None:
         return COMMANDS
     else:
         return COMMANDS[command]
 
 
+def list_commands():
+    """List all available commands
+    :returns: List containing the names of the commands
+    :rtype:  list
+
+    >>> len(list_commands()) > 0
+    True
+    >>> type(list_commands()) is list
+    True
+    >>> 'add' in list_commands() and 'open' in list_commands()
+    True
+    >>> 'default' in list_commands()
+    False
+    >>> 'external' in list_commands()
+    False
+    """
+    return [
+        cmd for cmd in get_commands().keys()
+        if cmd not in ['default', 'external']
+    ]
+
+
 def get_args():
+    """
+    Get general command line arguments.
+    """
     global ARGS
-    global logger
     return ARGS
 
 
@@ -53,7 +102,7 @@ def get_default_parser():
     if DEFAULT_PARSER is None:
         DEFAULT_PARSER = argparse.ArgumentParser(
             formatter_class=argparse.RawTextHelpFormatter,
-            description=""
+            description="Simple documents administration program"
         )
     return DEFAULT_PARSER
 
@@ -72,23 +121,36 @@ def get_subparsers():
     return SUBPARSERS
 
 
+def get_command_class_by_name(name):
+    """This returns returns a command class ready to be initialised
+
+    :param name: Name of the command, e.g., add
+    :type  name: str
+    :returns: A command not initialized
+    :rtype: panplot.commands.Command
+    """
+    exec("import panplot.commands.%s" % (name))
+    return eval("panplot.commands.%s.Command" % name)
+
+
 def init_internal_commands():
     global COMMAND_NAMES
     global logger
     commands = dict()
     cmd = None
-    logger.debug("Initializing commands")
+    logger.debug("Initializing internal commands")
     for command in COMMAND_NAMES:
         logger.debug(command)
-        exec("from .%s import %s" % (command, command.capitalize()))
-        cmd = eval(command.capitalize())()
+        cmd = get_command_class_by_name(command)()
         cmd.init()
         commands[command] = cmd
     return commands
 
 
 def init_external_commands():
-    from .external import External
+    import glob
+    from panplot.commands.external import Command as External
+    logger.debug("Initializing external commands")
     commands = dict()
     paths = []
     paths.append(panplot.config.get_scripts_folder())
@@ -98,21 +160,30 @@ def init_external_commands():
         if len(scripts):
             for script in scripts:
                 cmd = External()
+                logger.debug(script)
                 cmd.init(script)
                 commands[cmd.get_command_name()] = cmd
+    logger.debug("Initializing external commands done")
     return commands
 
 
-def init():
-    if get_commands() is not None:
-       raise RuntimeError("Commands are already initialised")
+def init_commands():
+    """Initialize all the commands
+    """
     commands = dict()
     commands.update(init_internal_commands())
     commands.update(init_external_commands())
     set_commands(commands)
+
+
+def init():
+    import argcomplete
+    if get_commands() is not None:
+        raise RuntimeError("Commands are already initialised")
+    init_commands()
     # autocompletion
     argcomplete.autocomplete(get_default_parser())
-    return commands
+    return get_commands()
 
 
 def main(input_args=[]):
@@ -120,24 +191,61 @@ def main(input_args=[]):
     # Parse arguments
     args = get_default_parser().parse_args(input_args or None)
     set_args(args)
+    logger.debug("running main")
     commands["default"].main()
+
+
+def init_and_return_parser():
+    """This function is here for the automatic documentation of the
+    subcommands.
+    :returns: General command line parser
+    """
+    try:
+        init()
+    except Exception:
+        pass
+    finally:
+        return get_default_parser()
 
 
 class Command(object):
 
+    parser = None
+    args = None
+
     def __init__(self):
         self.default_parser = get_default_parser()
-        self.parser = None
-        self.args = None
         self.subparsers = get_subparsers()
         self.logger = logging.getLogger(self.__class__.__name__)
-        self.config = panplot.config.get_configuration()
 
     def init(self):
         pass
 
     def main(self):
         pass
+
+    def add_search_argument(self):
+        self.parser.add_argument(
+            "search",
+            help="Search query string",
+            nargs="?",
+            default=".",
+            action="store"
+        )
+
+    def add_git_argument(
+            self,
+            flags=['--git'],
+            help="Run command involving git",
+            action=None
+            ):
+        action = action or ('store_false' if panplot.config.get('use-git')
+                                else 'store_true')
+        self.parser.add_argument(
+            *flags,
+            help=help,
+            action=action
+        )
 
     def set_args(self, args):
         self.args = args
@@ -147,6 +255,15 @@ class Command(object):
 
     def set_subparsers(self, subparsers):
         self.subparsers = subparsers
+
+    def get_config(self):
+        """Get configuration for the whole panplot. It just simply retrieves the
+        general configuration using the main ``panplot.config`` API.
+
+        :returns: General configuration
+        :rtype: dict
+        """
+        return panplot.config.get_configuration()
 
     def get_parser(self):
         return self.parser
